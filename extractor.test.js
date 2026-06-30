a275240222db744ec5d07915bcedebbf2afda6bf
= <you@example.com>
2026-06-30 13:20:58 +0200

    Dry the test code.

--- extractor.test.js
+++ extractor.test.js
@@ -1,7 +1,41 @@
+const { describe } = require("node:test");
 const extractor = require("./extractor.js");
 const fs = require("node:fs");
 
-// TODO: DRY this mess
+/**
+ * This function adds tests from a json file.
+ * The json file must be array of test objects (test units).
+ *
+ * Each test object must contain "input" and "want" attributes.
+ * "input" should be string, containing sme text that we want to extract links from.
+ * "want" must be array of expected results.
+ *
+ * It may contain a "skip" boolean attribute, if true than that test is not run.
+ *
+ * @param {string} name The name of the test group.
+ * @param {string} testsFilePath Path to the json file.
+ * @param {extractor.LinkExtractor} linkExtractor Initialized extractor that will be used in the tests.
+ */
+function addJsonTests(name, testsFilePath, linkExtractor) {
+  describe(name, () => {
+    const testUnits = JSON.parse(
+      fs.readFileSync(testsFilePath, { encoding: "utf-8" }),
+    );
+
+    testUnits.forEach((unit) => {
+      if (unit.skip) {
+        return;
+      }
+
+      test(unit.name, () => {
+        const result = linkExtractor.extract(unit.input);
+        const links = result.getValid();
+        expect(links.length).toBe(unit.want.length);
+        links.forEach((link, i) => expect(link.value).toBe(unit.want[i]));
+      });
+    });
+  });
+}
 
 test("'http://example.com' is link", () => {
   const linkExtractor = new extractor.LinkExtractor();
@@ -27,115 +61,42 @@ test("check that simple links get extracted correctly", () => {
 });
 
 // Still simple but there is text around links and more than one link per input.
-test("links in text", () => {
-  const tests = JSON.parse(
-    fs.readFileSync("./test_data/links-in-text.json", { encoding: "utf-8" }),
-  );
-  const linkExtractor = new extractor.LinkExtractor();
-
-  tests.forEach((test) => {
-    if (test.skip) {
-      return;
-    }
-    const result = linkExtractor.extract(test.input);
-    expect(result.links.length).toBe(test.want.length);
-    result.links.forEach((link, i) => expect(link.value).toBe(test.want[i]));
-  });
-});
+addJsonTests(
+  "links in text",
+  "./test_data/links-in-text.json",
+  new extractor.LinkExtractor(),
+);
 
 // We need to handle cases where the URL is surrounded by different kind of brackets
-describe("brackets", () => {
-  const testUnits = JSON.parse(
-    fs.readFileSync("./test_data/links-in-brackets.json", {
-      encoding: "utf-8",
-    }),
-  );
-  const linkExtractor = new extractor.LinkExtractor();
-
-  testUnits.forEach((unit) => {
-    if (unit.skip) {
-      return;
-    }
-    test(unit.name, () => {
-      const result = linkExtractor.extract(unit.input);
-      expect(result.links.length).toBe(unit.want.length);
-      result.links.forEach((link, i) => expect(link.value).toBe(unit.want[i]));
-    });
-  });
-});
+addJsonTests(
+  "links in brackets",
+  "./test_data/links-in-brackets.json",
+  new extractor.LinkExtractor(),
+);
 
 // Test the AppendPotentialSeparators postprocessor
-describe("append separators postprocessor", () => {
-  const testUnits = JSON.parse(
-    fs.readFileSync("./test_data/append-separators.json", {
-      encoding: "utf-8",
-    }),
-  );
-
-  const linkExtractor = new extractor.LinkExtractor();
-  linkExtractor.postprocessorChain.add(
-    new extractor.AppendPotentialSeparators(),
-  );
-
-  testUnits.forEach((unit) => {
-    if (unit.skip) {
-      return;
-    }
-    test(unit.name, () => {
-      const result = linkExtractor.extract(unit.input);
-      expect(result.links.length).toBe(unit.want.length);
-      result.links.forEach((link, i) => expect(link.value).toBe(unit.want[i]));
-    });
-  });
-});
+addJsonTests(
+  "append separators postprocessor",
+  "./test_data/append-separators.json",
+  (() => {
+    const linkExtractor = new extractor.LinkExtractor();
+    linkExtractor.postprocessorChain.add(
+      new extractor.AppendPotentialSeparators(),
+    );
+    return linkExtractor;
+  })(),
+);
 
 // Links that are split over multiple lines
-describe("newline splits", () => {
-  const testUnits = JSON.parse(
-    fs.readFileSync("./test_data/newline-splits.json", {
-      encoding: "utf-8",
-    }),
-  );
-
-  // This needs the PDF chain.
-  const linkExtractor = new extractor.LinkExtractor(
-    extractor.getPdfPostprocessorChain(),
-  );
-
-  testUnits.forEach((unit) => {
-    if (unit.skip) {
-      return;
-    }
-    test(unit.name, () => {
-      const result = linkExtractor.extract(unit.input);
-      const links = result.getValid();
-      expect(links.length).toBe(unit.want.length);
-      links.forEach((link, i) => expect(link.value).toBe(unit.want[i]));
-    });
-  });
-});
+addJsonTests(
+  "newline splits",
+  "./test_data/newline-splits.json",
+  new extractor.LinkExtractor(extractor.getPdfPostprocessorChain()),
+);
 
 // Links that are split over multiple lines
-describe("remove links contained in other links", () => {
-  const testUnits = JSON.parse(
-    fs.readFileSync("./test_data/links-in-links.json", {
-      encoding: "utf-8",
-    }),
-  );
-
-  const linkExtractor = new extractor.LinkExtractor(
-    extractor.getPdfPostprocessorChain(),
-  );
-
-  testUnits.forEach((unit) => {
-    if (unit.skip) {
-      return;
-    }
-    test(unit.name, () => {
-      const result = linkExtractor.extract(unit.input);
-      const links = result.getValid();
-      expect(links.length).toBe(unit.want.length);
-      links.forEach((link, i) => expect(link.value).toBe(unit.want[i]));
-    });
-  });
-});
+addJsonTests(
+  "remove links contained in other links",
+  "./test_data/links-in-links.json",
+  new extractor.LinkExtractor(extractor.getPdfPostprocessorChain()),
+);
